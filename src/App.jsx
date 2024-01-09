a71a169d369861c12ccd58eda01625717747ef19
osman kelifa <o_kelifa@hotmail.com>
2024-01-09 15:55:51 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <p className="text-center font-bold uppercase text-green-400 bg">Hi mom</p>;
+}
+
+export default App;
